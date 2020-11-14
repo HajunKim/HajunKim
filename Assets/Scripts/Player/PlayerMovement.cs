2a2ab2334c500427226f041c9faf8b31dbcfdd74
eunjin <ejchoi.infinyx@gmail.com>
2020-11-15 05:41:02 +0900

    update 2p player mode, see #46

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,11 +6,15 @@ namespace Nightmare
     public class PlayerMovement : PausibleObject
     {
         public float speed = 6f;            // The speed that the player will move at.
-
+        public float rotateSpeed = 4f;      // The spped of rotation
+        public bool useMouse;               // use mouse or not(single play)
 
         Vector3 movement;                   // The vector to store the direction of the player's movement.
         Animator anim;                      // Reference to the animator component.
         Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
+        float h;                            // up & down key amount.
+        float v;                            // left & right key amount.
+        string Hkeyname, Vkeyname;          // mapped key name of horizonal & vertical axis.(for multiplayer)
 #if !MOBILE_INPUT
         int floorMask;                      // A layer mask so that a ray can be cast just at gameobjects on the floor layer.
         float camRayLength = 100f;          // The length of the ray from the camera into the scene.
@@ -22,6 +26,18 @@ namespace Nightmare
             // Create a layer mask for the floor layer.
             floorMask = LayerMask.GetMask ("Floor");
 #endif
+            GameObject player2P = GameObject.Find("Player2P");
+    
+            // Set Cameramovement
+            if (!useMouse)
+            {
+                GameObject camera = GameObject.FindGameObjectWithTag("MainCamera");
+                camera.GetComponent<MyCameraFollow>().enabled = false;
+            }
+
+            // Set key mappings
+            Hkeyname = this.gameObject.name.Equals("Player") ? "Horizontal" : "Horizontal2P";
+            Vkeyname = this.gameObject.name.Equals("Player") ? "Vertical" : "Vertical2P";
 
             // Set up references.
             anim = GetComponent <Animator> ();
@@ -39,11 +55,10 @@ namespace Nightmare
         {
             if (isPaused)
                 return;
-            float h, v;
-            
+ 
             // Store the input axes.
-            h = CrossPlatformInputManager.GetAxisRaw("Horizontal");
-            v = CrossPlatformInputManager.GetAxisRaw("Vertical");
+            h = CrossPlatformInputManager.GetAxisRaw(Hkeyname);
+            v = CrossPlatformInputManager.GetAxisRaw(Vkeyname);
             
             // Move the player around the scene.
             Move (h, v);
@@ -72,26 +87,41 @@ namespace Nightmare
         void Turning ()
         {
 #if !MOBILE_INPUT
-            // Create a ray from the mouse cursor on screen in the direction of the camera.
-            Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
-
-            // Create a RaycastHit variable to store information about what was hit by the ray.
-            RaycastHit floorHit;
-
-            // Perform the raycast and if it hits something on the floor layer...
-            if(Physics.Raycast (camRay, out floorHit, camRayLength, floorMask))
+            
+            if (!useMouse)
             {
-                // Create a vector from the player to the point on the floor the raycast from the mouse hit.
-                Vector3 playerToMouse = floorHit.point - transform.position;
+                if (h == 0 && v == 0)
+                {
+                    return; // no movement, no turn
+                }
+                Quaternion newRotation = Quaternion.LookRotation(movement);
 
-                // Ensure the vector is entirely along the floor plane.
-                playerToMouse.y = 0f;
+                playerRigidbody.rotation = Quaternion.Slerp(playerRigidbody.rotation, newRotation, rotateSpeed * Time.deltaTime);
+            }
+            else
+            {
+                // mouse input
+                // Create a ray from the mouse cursor on screen in the direction of the camera.
+                Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-                // Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
-                Quaternion newRotatation = Quaternion.LookRotation (playerToMouse);
+                // Create a RaycastHit variable to store information about what was hit by the ray.
+                RaycastHit floorHit;
 
-                // Set the player's rotation to this new rotation.
-                playerRigidbody.MoveRotation (newRotatation);
+                // Perform the raycast and if it hits something on the floor layer...
+                if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+                {
+                    // Create a vector from the player to the point on the floor the raycast from the mouse hit.
+                    Vector3 playerToMouse = floorHit.point - transform.position;
+
+                    // Ensure the vector is entirely along the floor plane.
+                    playerToMouse.y = 0f;
+
+                    // Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
+                    Quaternion newRotatation = Quaternion.LookRotation(playerToMouse);
+
+                    // Set the player's rotation to this new rotation.
+                    playerRigidbody.MoveRotation(newRotatation);
+                }
             }
 #else
 
