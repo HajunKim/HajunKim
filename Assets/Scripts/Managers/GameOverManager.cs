0f4a377a16aa3306e313bb1c05492630b7936eed
eunjin <ejchoi.infinyx@gmail.com>
2020-11-15 00:52:43 +0900

    Survival Shooting Game init

---
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+namespace Nightmare
+{
+    public class GameOverManager : MonoBehaviour
+    {
+        private PlayerHealth playerHealth;
+        private float restartDelay = 5f;
+        
+        Animator anim;
+        float restartTimer;
+
+        LevelManager lm;
+        private UnityEvent listener;
+
+        void Awake()
+        {
+            playerHealth = FindObjectOfType<PlayerHealth>();
+            anim = GetComponent<Animator>();
+        }
+        void Update()
+        {
+            // If the player has run out of health...
+            if (playerHealth.currentHealth <= 0)
+            {
+                // ... tell the animator the game is over.
+                anim.SetTrigger("GameOver");
+
+                // .. increment a timer to count up to restarting.
+                restartTimer += Time.deltaTime;
+
+                // .. if it reaches the restart delay...
+                if (restartTimer >= restartDelay)
+                {
+                    // .. then reload the currently loaded level.
+                    Scene scene = SceneManager.GetActiveScene();
+                    SceneManager.LoadScene(scene.name);
+                }
+            }
+        }
+
+        //void Awake ()
+        //{
+        //    playerHealth = FindObjectOfType<PlayerHealth>();
+        //    anim = GetComponent <Animator> ();
+        //    lm = FindObjectOfType<LevelManager>();
+        //    EventManager.StartListening("GameOver", ShowGameOver);
+        //}
+
+        //void OnDestroy()
+        //{
+        //    EventManager.StopListening("GameOver", ShowGameOver);
+        //}
+
+        //void ShowGameOver()
+        //{
+        //    anim.SetBool("GameOver", true);
+        //}
+
+        //private void ResetLevel()
+        //{
+        //    ScoreManager.score = 0;
+        //    LevelManager lm = FindObjectOfType<LevelManager>();
+        //    Scene scene = SceneManager.GetActiveScene();
+        //    SceneManager.LoadScene(scene.name);
+        //    lm.LoadInitialLevel();
+        //    anim.SetBool("GameOver", false);
+        //    playerHealth.ResetPlayer();
+        //}
+    }
+}
