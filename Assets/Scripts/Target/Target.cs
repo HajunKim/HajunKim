b5f1a88d123d79337a7affa0cba9e1779b8da915
Eunjin Choi <jech@kaist.ac.kr>
2020-12-07 02:24:18 +0900

    - target activated when same pitch singing successive 50 frames - target material change to transparent - target alpha is changing depend on the progress of singing ! default = 0.8 -> 1.0 when target activated

--- Assets/Scripts/Target/Target.cs
+++ Assets/Scripts/Target/Target.cs
@@ -21,6 +21,9 @@ namespace Nightmare
         AudioSource audioSource;
         public AudioClip vitalizedSound;
         public string targetNote;
+        public string prevNote; // log of previous note
+        private int currentCnt = 0; // count number of frames with success of targetNote 
+        public int targetActivateCnt = 50; // number of frames for targetActivate
 
         Light circleLight;
         float y = 0.0f;
@@ -44,7 +47,7 @@ namespace Nightmare
             LineDrawer = GetComponent<LineRenderer>();
             audioSource = GetComponent<AudioSource>();
             //soundModule = GetComponent<SoundModule>();
-            //LineDrawer.material = new Material(Shader.Find("Particles/Alpha Blended"));
+            //LineDrawer.material = new Materia l(Shader.Find("Particles/Alpha Blended"));
             //LineDrawer.SetColors(Color.blue, Color.blue);
 
         }
@@ -101,84 +104,59 @@ namespace Nightmare
         {            
             if (playerInRange)
             {
+                // light turn on
+                circleLight.enabled = true;
+                LineDrawer.enabled = true;
+                Theta = 0f;
+                Size = (int)((1f / ThetaScale) + 1f);
+                LineDrawer.SetVertexCount(Size);
+                for (int i = 0; i < Size; i++)
+                {
+                    Theta += (2.0f * Mathf.PI * ThetaScale);
+                    float x = radius * Mathf.Cos(Theta);
+                    float z = radius * Mathf.Sin(Theta);
+                    LineDrawer.SetPosition(i, new Vector3(x + transform.position[0], transform.position[1] - 100.0f + y, z + transform.position[2]));
+                }
+
+                // target coloring
                 if (!isVitalized)
                 {
                     float value = SoundModule.Instance.GetPlayerNoteMappingValue();
                     string note = SoundModule.Instance.GetPlayerNote();
+                    if (note[note.Length - 1] < '2' || note[note.Length-1] > '5') return; // freq filtering by octave
                     note = note.Substring(0, note.Length - 1); //we are not using octave info
                     if (note.Length > 0)
                     {
                         int r = noteColorMapping[note].Item1;
                         int g = noteColorMapping[note].Item2;
                         int b = noteColorMapping[note].Item3;
-                        targetColor.material.color = new Color(r,g,b);
-
+                        
                         if (String.Equals(note, targetNote))
                         {
-                            Debug.Log("Target Note : "+targetNote + " activated!");
-                            isVitalized = true;
-                            audioSource.clip = vitalizedSound;
-                            audioSource.Play();
+                            if (currentCnt == 0) currentCnt = 1;
+                            else
+                            {
+                                if (String.Equals(note, prevNote)) currentCnt++;
+                                else currentCnt = 0;
+                            }
+                            Debug.Log(currentCnt);
+                            if (targetActivateCnt == currentCnt)
+                            {
+                                Debug.Log("Target Note : " + targetNote + " activated!");
+                                isVitalized = true;
+                                audioSource.clip = vitalizedSound;
+                                audioSource.Play();
+                            }
+                            
                         }
+                        Color c = new Color(r, g, b);
+                        c.a = 0.8f+ 0.2f*(float)currentCnt / (float)targetActivateCnt; // 0 ~ 1
+                        targetColor.material.color = c;
+                        //targetColor.material.SetFloat("_Metallic", alpha);
+
+                        prevNote = note;
                     }
-                    //float value = SoundModule.Instance.GetPlayerNoteMappingValue();
-                    //string note = SoundModule.Instance.GetPlayerNote();
-                    //int r = noteColorMapping[note].Item1;
-                    //int g = noteColorMapping[note].Item2;
-                    //int b = noteColorMapping[note].Item3;
-                    ////targetColor.material.color = new Color(, 255 * value, 255 * value);
-                    //Debug.Log(note + r + g + b);
-                    //if (0.0f <= value && value < 0.0833f) // C
-                    //{
-                    //    targetColor.material.color = new Color(255, 255, 255);
-                    //}
-                    //else if (0.0833f <= value && value < 0.166f) // C#
-                    //{
-                    //    targetColor.material.color = new Color(128, 255, 0);
-                    //}
-                    //else if (0.166f <= value && value < 0.25f) // D
-                    //{
-                    //    targetColor.material.color = new Color(51, 102, 0);
-                    //}
-                    //else if (0.25f <= value && value < 0.315f) // D#
-                    //{
-                    //    targetColor.material.color = new Color(255, 0, 127);
-                    //}
-                    //else if (0.315f <= value && value < 0.416f) // E
-                    //{
-                    //    targetColor.material.color = new Color(204, 0, 0);
-                    //}
-                    //else if (0.416 <= value && value < 0.5f) // F
-                    //{
-                    //    targetColor.material.color = new Color(102, 0, 0);
-                    //}
-                    //else if (0.5f <= value && value < 0.583f) // F#
-                    //{
-                    //    targetColor.material.color = new Color(0, 204, 204);
-                    //}
-                    //else if (0.583f <= value && value < 0.666f) // G
-                    //{
-                    //    targetColor.material.color = new Color(0, 0, 255);
-                    //}
-                    //else if (0.666f <= value && value < 0.75f) // G#
-                    //{
-                    //    targetColor.material.color = new Color(0, 0, 153);
-                    //    isVitalized = true;
-                    //    audioSource.clip = vitalizedSound;
-                    //    audioSource.Play();
-                    //}
-                    //else if (0.75f <= value && value < 0.833f) // A
-                    //{
-                    //    targetColor.material.color = new Color(0, 0, 51);
-                    //}
-                    //else if (0.833f <= value && value < 0.916f) // A#
-                    //{
-                    //    targetColor.material.color = new Color(160, 160, 160);
-                    //}
-                    //else if (0.916f <= value && value < 1.0f) // B
-                    //{
-                    //    targetColor.material.color = new Color(0, 0, 0);
-                    //}
+                    
                 }
                 //if(isVitalized)
                 //{
@@ -187,17 +165,7 @@ namespace Nightmare
                 //targetColor.material.color = new Color(255, 255, 255);
                 //new Color(233, 79, 55);
                 //Color(233f/255f, 79f/255f, 55f/255f);
-                circleLight.enabled = true;
-                LineDrawer.enabled = true; 
-                Theta = 0f;
-                Size = (int)((1f / ThetaScale) + 1f);
-                LineDrawer.SetVertexCount(Size); 
-                for(int i = 0; i < Size; i++){          
-                    Theta += (2.0f * Mathf.PI * ThetaScale);         
-                    float x = radius * Mathf.Cos(Theta);
-                    float z = radius * Mathf.Sin(Theta);          
-                    LineDrawer.SetPosition(i, new Vector3(x + transform.position[0], transform.position[1]-100.0f+y, z+transform.position[2]));
-                 }
+                
             }
             else
             {
