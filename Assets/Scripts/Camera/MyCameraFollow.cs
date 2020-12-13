ce7a56c5f256f8d5158dbf49a6d57cbce73e62db
byeol2 <byeol2@kaist.ac.kr>
2020-12-14 03:38:50 +0900

    Characters importing tries (AC, import folder, etc) & Scene changed a little bit such as Game Background asset coloring

--- Assets/Scripts/Camera/MyCameraFollow.cs
+++ Assets/Scripts/Camera/MyCameraFollow.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MyCameraFollow : MonoBehaviour
 {
     public Transform target;
-    public float smoothing = 5f;
+    public float smoothing = 3f;
 
     Vector3 offset;
 
